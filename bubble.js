091af834dd3ddf76c3b0426b630cd59da9a157d3
st234pa <syoon1@stuy.edu>
2017-03-06 13:53:48 -0500

    ?

--- bubble.js
+++ bubble.js
@@ -1,10 +1,10 @@
 var pic = document.getElementById("vimage");
 var clr = document.getElementById("clr");
 
-var makeCircle = function() {
+var makeCircle = function(x, y) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", 20);
-    c.setAttribute("cy", 20);
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
     c.setAttribute("r", 15);
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
@@ -12,8 +12,10 @@ var makeCircle = function() {
 }
 
 
-var addCircle = function() {
-    var c = makeCircle();
+var addCircle = function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var c = makeCircle(x,y);
     pic.appendChild(c);
 }
 
